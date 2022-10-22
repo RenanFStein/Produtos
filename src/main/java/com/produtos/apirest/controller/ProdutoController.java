04d6d74db734a078a14bdf41e9cde556cb17f081
RenanFStein <renan_stein@hotmail.com>
2022-10-21 22:46:59 -0300

    Atualização do Interceptador

--- src/main/java/com/produtos/apirest/controller/ProdutoController.java
+++ src/main/java/com/produtos/apirest/controller/ProdutoController.java
@@ -32,18 +32,7 @@ public class ProdutoController {
 	@Autowired
 	ProdutoRepository produtoRepository;
 
-	// Interceptador de Erros para retornar mensagem em caso de erro.
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public Map<String, String> handleValidationException(MethodArgumentNotValidException ex){
-		Map<String, String> errors = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach(error -> {
-			String fieldName = ((FieldError) error).getField();
-			String errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-		});
-		return errors;
-	}
+
 
 	// GET para listar todos os produtos.
 	@ApiOperation(value="Retorna uma lista de Produtos")
